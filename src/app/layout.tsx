1ad8f387eb1d868ecdcb1ac4ee912f75b31b4fdf
VirtualVince <virtualvince2020@gmail.com>
2023-07-30 11:56:52 -0400

    meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'virtualvince.ca',
+  title: 'Virtual Vince',
   description: 'Created by Virtual Vince',
 }
 
